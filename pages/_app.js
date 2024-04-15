5bbb446c822eb20a89e716e21c37ff523c13afa6
Saini-Gaurav <gauravsaini905888@gmail.com>
2024-04-15 16:47:30 +0530

    Header, Footer & Crousel Section is designed

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,23 @@
+import Header from "@/components/Header";
 import "@/styles/globals.css";
+import "bootstrap/dist/css/bootstrap.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import '../public/css/templatemo.css'
+import Footer from "@/components/Footer";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap");
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
